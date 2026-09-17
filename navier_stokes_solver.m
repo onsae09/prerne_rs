240bf89eb2b4088007e079f3b4e3febbb033ab51
onsae09 <onsae09@gmail.com>
2026-09-18 01:10:05 +0900

    refactor: simplify air properties calculation and improve function parameters in navier_stokes_solver.m

--- navier_stokes_solver.m
+++ navier_stokes_solver.m
@@ -25,12 +25,18 @@ M_species = [28.0134, 31.9988, 39.948] * 1e-3;
 M_air = x_species * M_species';
 R = R_u / M_air;
 
-% NASA 9 coefficients
+% NASA 9 coefficients: N2, O2, Ar
 A = [
     2.210371497e4, -3.818461820e2, 6.082738360, -8.530914410e-3, 1.384646189e-5, -9.625793620e-9, 2.519705809e-12, 7.108460860e2, -1.076003744e1;
    -3.425563420e4,  4.847000970e2, 1.119010961,  4.293889240e-3, -6.836300520e-7, -2.023372700e-9, 1.039040018e-12, -3.391454870e3, 1.849699470e1;
     0, 0, 2.5, 0, 0, 0, 0, -7.453750000e2, 4.37967491
 ];
+
+% 혼합공기 NASA 계수 미리 계산
+A_air = x_species * A;
+
+% 질량기준 변환계수
+R_factor = R_u / M_air;
 
 % 초기 설정값
 u = zeros(Nx+2, Ny+2, Nz+2, 3);
@@ -42,7 +48,7 @@ T = ones(Nx+2, Ny+2, Nz+2) * T_initial;
 p = ones(Nx+2, Ny+2, Nz+2) * p_initial;
 rho = p ./ (R .* T);
 
-[C_p, h] = air_properties(T, x_species, M_species, A, R_u);
+[C_p, h] = air_properties(T, A_air, R_factor);
 
 e = h - R .* T;
 E = rho .* (e + 0.5 .* sum(u.^2, 4));
@@ -50,18 +56,13 @@ E = rho .* (e + 0.5 .* sum(u.^2, 4));
 r = [0 0 0];
 g = -G * m * (r + r0) ./ vecnorm(r + r0, 2, 2).^3;
 
-function [Cp, h] = air_properties(T, x, M, A, R_u)
+function [Cp, h] = air_properties(T, a, R_factor)
     assert(all(T(:) >= 200 & T(:) <= 400), 'Temperature must be between 200 K and 400 K.');
 
-    M_air = x * M';
+    Cp_R = a(1)./T.^2 + a(2)./T + a(3) + a(4).*T + a(5).*T.^2 + a(6).*T.^3 + a(7).*T.^4;
 
-    cpR = @(a) a(1)./T.^2 + a(2)./T + a(3) + a(4).*T + a(5).*T.^2 + a(6).*T.^3 + a(7).*T.^4;
+    h_RT = -a(1)./T.^2 + a(2).*log(T)./T + a(3) + a(4).*T./2 + a(5).*T.^2./3 + a(6).*T.^3./4 + a(7).*T.^4./5 + a(8)./T;
 
-    hRT = @(a) -a(1)./T.^2 + a(2).*log(T)./T + a(3) + a(4).*T./2 + a(5).*T.^2./3 + a(6).*T.^3./4 + a(7).*T.^4./5 + a(8)./T;
-
-    Cp_R = x(1).*cpR(A(1,:)) + x(2).*cpR(A(2,:)) + x(3).*cpR(A(3,:));
-    h_RT = x(1).*hRT(A(1,:)) + x(2).*hRT(A(2,:)) + x(3).*hRT(A(3,:));
-
-    Cp = Cp_R .* R_u ./ M_air; % J/(kg*K)
-    h = h_RT .* R_u .* T ./ M_air; % J/kg
+    Cp = Cp_R .* R_factor;
+    h = h_RT .* R_factor .* T;
 end
